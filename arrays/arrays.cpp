26db2210c362db651e40504359fe4aabf1d46d44
Jerimiah Huston <jhuston357@live.com>
2019-03-09 19:09:34 -0500

    array traversal code

---
+++ arrays/arrays.cpp
@@ -0,0 +1,30 @@
+//Array Operations
+//Traverse
+//Insertion
+//Deletion
+//Search
+//Update
+
+#include <iostream>
+using namespace std;
+
+main()
+{
+
+  //number of array elements
+  int n = 8;
+
+  //initialize array
+  int array[] = {2,8,4,17,11,12,1,8};
+
+  //traverse Array
+  for(int i=0; i<n; i++)
+  {
+
+    cout << "Array[" << i << "] = " << array[i] << " !\n";
+
+  }
+
+  return 0;
+
+}
