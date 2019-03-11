908f78916ab6f67f8c3b240b21540b838e4270ed
Jerimiah Huston <afknscientist@gmail.com>
2019-03-10 23:20:49 -0400

    changed traverse to a function and added insertion function

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -8,23 +8,94 @@
 #include <iostream>
 using namespace std;
 
-main()
+//function declarations
+void traverse(int array[],int len);
+int * insert(int array[],int newitem,int pos,int *len, int *size);
+
+
+//main
+int main()
 {
 
   //number of array elements
-  int n = 8;
+  int * len = new int;
+  *len = 8;
+
+  //array size
+  int *size = new int;
+  *size = 8;
 
   //initialize array
-  int array[] = {2,8,4,17,11,12,1,8};
+  int poparray[] = {2,8,4,17,11,12,1,8};
+  int *array;
+  array = poparray;
+
+  traverse(array,*len);
+  array = insert(array,10,5,len,size)
+  cout << "\n";
+  traverse(array,*len);
+
+  return 0;
+
+}
+
+//functions
+
+//traverse an array
+void traverse(int array[],int len)
+{
 
   //traverse Array
-  for(int i=0; i<n; i++)
+  for(int i=0; i<len; i++)
   {
 
     cout << "Array[" << i << "] = " << array[i] << " !\n";
 
   }
 
-  return 0;
+}
+
+int * insert(int * array,int newitem,int pos,int *len, int *size)
+{
+
+  //if array is full double the size
+  if(*len == *size)
+  {
+
+    *size = (*size)*2;
+
+  }
+
+  //add one on to length because we are inserting
+  *len = *len + 1;
+
+  //initialize new array to insert new item
+  int *newarray = new int[*size];
+
+  //insert new item because we know the position
+  newarray[pos] = newitem;
+
+  // loop through and populate the new array with the old array data
+  for(int i=0; i < *len; i++)
+  {
+
+
+    if(i<pos)//fill all less than than new item pos
+    {
+
+      newarray[i] = array[i];
+
+    }
+    else if (i > pos) // fill all over new item position
+    {
+
+      newarray[i] = array[i-1];
+
+    }
+
+
+  }
+
+  return newarray;
 
 }
